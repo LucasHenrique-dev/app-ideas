b804fbefda72fc1aa073658f71f922f281524e2d
Lucas Henrique Santos Neto <lucas.hneto@hotmail.com>
2022-02-24 22:43:22 -0300

    update
    basics features implemented

--- projects/Border-radius_Previewer/scripts/box.js
+++ projects/Border-radius_Previewer/scripts/box.js
@@ -1,7 +1,34 @@
-const border_radius_button = document.querySelector("[data-button-border_radius]")
-const div_controladores = document.querySelector("[data-div-controladores]")
-const controladores = div_controladores.children
+(() => {
+    const border_radius_button = document.querySelector("[data-button-border_radius]")
+    const div_controladores = document.querySelector("[data-div-controladores]")
 
-border_radius_button.addEventListener("click", () => {
-    console.log(controladores[0])
-})
+    const controladores = div_controladores.children
+    const box_style = border_radius_button.style
+
+    for (const controlador of controladores) {
+        controlador.addEventListener("input", () => {
+            if (controlador.id == "number_0") {
+                box_style.borderTopLeftRadius = `${controlador.value}px`
+            } else if (controlador.id == "number_1") {
+                box_style.borderTopRightRadius = `${controlador.value}px`
+            } else if (controlador.id == "number_2") {
+                box_style.borderBottomLeftRadius = `${controlador.value}px`
+            } else if (controlador.id == "number_3") {
+                box_style.borderBottomRightRadius = `${controlador.value}px`
+            }
+        })
+    }
+
+    border_radius_button.addEventListener("click", () => {
+        const top_left = !controladores[0].value ? "5" : controladores[0].value,
+        top_right = !controladores[1].value ? "5" : controladores[1].value,
+        bottom_left = !controladores[2].value ? "5" : controladores[2].value,
+        bottom_right = !controladores[3].value ? "5" : controladores[3].value
+
+        const copyText = `border-radius: ${top_left}px ${top_right}px ${bottom_right}px ${bottom_left}px`
+
+        navigator.clipboard.writeText(`${copyText}`).then(() => {
+            window.alert("Estilo de Borda Copiado!")
+        })
+    })
+})()
